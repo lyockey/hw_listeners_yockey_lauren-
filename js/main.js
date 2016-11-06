5647c666fb144649d6a421bff7a0006fe19ab903
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-06 13:11:43 -0800

    make paragraph text display click count

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var clickTracker = 1;
 heading.addEventListener('click', function () {
     var myElement = document.createElement('p');
 
-    myElement.innerHTML = 'This is click number x, where x is 1 on the first click, 2 on the second click, 3 on the third, etc.';
+    myElement.innerHTML = 'This is click number ' + clickTracker;
     clickTracker = clickTracker + 1;
 
     document.getElementsByTagName('header')[0].appendChild(myElement);
