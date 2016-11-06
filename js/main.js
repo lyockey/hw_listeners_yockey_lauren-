ecb4f3b7f66be5459ef4be84cb8678615d505701
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-06 13:10:32 -0800

    use a variable to record the number of clicks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var heading = document.getElementsByTagName('h1')[0];
+var clickTracker = 1;
 
 heading.addEventListener('click', function () {
     var myElement = document.createElement('p');
 
     myElement.innerHTML = 'This is click number x, where x is 1 on the first click, 2 on the second click, 3 on the third, etc.';
+    clickTracker = clickTracker + 1;
 
     document.getElementsByTagName('header')[0].appendChild(myElement);
 });
